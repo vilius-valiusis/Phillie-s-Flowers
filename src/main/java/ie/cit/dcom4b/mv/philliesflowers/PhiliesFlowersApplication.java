1f7f70ac16b98b2462332465d8fd86d55b92e141
VV <VV@VIL-DESKTOP>
2017-11-21 13:38:27 +0000

    INIT

---
+++ src/main/java/ie/cit/dcom4b/mv/philliesflowers/PhiliesFlowersApplication.java
@@ -0,0 +1,12 @@
+package ie.cit.dcom4b.mv.philliesflowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhiliesFlowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhiliesFlowersApplication.class, args);
+	}
+}
---
+++ src/test/java/ie/cit/dcom4b/mv/philliesflowers/PhiliesFlowersApplicationTests.java
@@ -0,0 +1,16 @@
+package ie.cit.dcom4b.mv.philliesflowers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhiliesFlowersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
