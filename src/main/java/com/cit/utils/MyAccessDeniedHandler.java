a4c0fc8fab3a6be7e1743fb467c4cf5957fd4acc
admin <admin@MattPC>
2017-12-08 18:00:27 +0000

    new exception handler and other small changes

---
+++ src/main/java/com/cit/utils/MyAccessDeniedHandler.java
@@ -0,0 +1,40 @@
+package com.cit.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// This class handles error handling based on failed http requests. A good example is if you get a 403 error the exception will be handled.
+// This will also be used when checking params sent back from a post request i.e if the correct login details were used for backend.
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static Logger logger = LoggerFactory.getLogger(MyAccessDeniedHandler.class);
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException e) throws IOException, ServletException {
+
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            logger.info("User '" + auth.getName()
+                    + "' attempted to access the protected URL: "
+                    + httpServletRequest.getRequestURI());
+        }
+
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/403");
+
+    }
+}
